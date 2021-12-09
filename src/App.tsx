a4285cccacde25192011e39484337e954f37b123
hypercubist <qpinguz@gmail.com>
2021-12-09 10:09:07 +0900

    create Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,6 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-  color: ${(props) => props.theme.textColor};
-`;
+import Router from "./Router";
 
 function App() {
-  const [value, setValue] = useState("");
-
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello", value);
-  };
-
-  return (
-    <div>
-      <Container>
-        <h1>Hello</h1>
-      </Container>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log in</button>
-      </form>
-    </div>
-  );
+  return <Router></Router>;
 }
-
 export default App;
